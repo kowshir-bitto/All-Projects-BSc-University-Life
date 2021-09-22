fc062c144babec07f21551a0dd0db1be294a38c6
Abu Kowshir Bitto <58230610+kowshir-bitto@users.noreply.github.com>
2021-09-22 21:37:30 +0600

    Delete Fund Voice/admin directory

--- Fund Voice/admin/includes/logout.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if(isset($_GET['logout']) && $_GET['logout'] == 'done') {
-    session_start(); 
-    session_unset(); 
-    session_destroy(); 
-    header("Location: ../index.php");
-    exit();
-}
